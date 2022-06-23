2ae0a93d8b10e44b301760fa50a8c72055afac57
ilker61 <jakyuer@gmail.com>
2022-06-23 23:07:28 +0200

    23.06.22 gün sonuu

--- src/test/java/pages/US_05_06_07_Page.java
+++ src/test/java/pages/US_05_06_07_Page.java
@@ -1,4 +1,11 @@
 package pages;
 
+import org.openqa.selenium.support.PageFactory;
+import utilities.Driver;
+
 public class US_05_06_07_Page {
+    public US_05_06_07_Page () {PageFactory.initElements(Driver.getDriver(), this);}
+
+
+
 }
