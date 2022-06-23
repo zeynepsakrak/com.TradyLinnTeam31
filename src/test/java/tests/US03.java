f2522a3aeffa7c8171129f3e1f9115b185f7e6b2
Resul Dagci <resuldagci27@gmail.com>
2022-06-23 09:06:54 +0200

    23.06.22

--- src/test/java/tests/US03.java
+++ src/test/java/tests/US03.java
@@ -1,5 +1,13 @@
 package tests;
 
+import org.testng.annotations.Test;
+import utilities.ConfigReader;
+import utilities.Driver;
+
 public class US03 {
 
+    @Test
+    public void US03_TC01(){
+        Driver.getDriver().get(ConfigReader.getProperty("tradylinnUrl"));
+    }
 }
