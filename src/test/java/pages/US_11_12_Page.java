d7cd09d53f2e83a75417230ea2a8c4177edc6dbd
Oner Tekin <tekinoner37@gmail.com>
2022-06-23 10:10:34 +0300

    23.06.22

---
+++ src/test/java/pages/US_11_12_Page.java
@@ -0,0 +1,12 @@
+package pages;
+
+import org.openqa.selenium.support.PageFactory;
+import utilities.Driver;
+
+public class US_11_12_Page {
+
+        public US_11_12_Page(){
+            PageFactory.initElements(Driver.getDriver(),this);
+
+        }
+}
