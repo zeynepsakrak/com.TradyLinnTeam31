c17aec402cc7f377133d45826d01c1228640714b
zeynepsakrak <100724081+zeynepsakrak@users.noreply.github.com>
2022-06-25 16:31:47 +0300

    6/25/22

--- src/test/java/pages/US_08_09_10_Page.java
+++ src/test/java/pages/US_08_09_10_Page.java
@@ -1,4 +1,15 @@
 package pages;
 
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.FindBy;
+import org.openqa.selenium.support.PageFactory;
+import utilities.Driver;
+
 public class US_08_09_10_Page {
+    public US_08_09_10_Page() {
+        PageFactory.initElements(Driver.getDriver(),this); }
+
+    @FindBy(xpath = "//*[@id=\"menu-item-1074\"]/a")
+    public WebElement hesabim;
+
 }
--- src/test/java/tests/ReusableMethods.java
+++ src/test/java/tests/ReusableMethods.java
@@ -5,6 +5,8 @@ import pages.Homepage;
 import utilities.ConfigReader;
 import utilities.Driver;
 
+import static tests.Login.login;
+
 public class ReusableMethods {
     static  JavascriptExecutor jse = (JavascriptExecutor) Driver.getDriver();
 
@@ -19,4 +21,23 @@ public class ReusableMethods {
             homepage.tradylinnIkonu.click();
         }
     }
+    public static void urun_ekle_menusune_gidilir(){
+        Homepage homepage=new Homepage();
+        login();
+        try {
+            Thread.sleep(3000);
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+        homepage.hesabim.click();
+    homepage.StoreManager.click();
+        try {
+            Thread.sleep(1000);
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+        homepage.urun.click();
+    homepage.yeniUrunEkle.click();
+    }
+
 }
