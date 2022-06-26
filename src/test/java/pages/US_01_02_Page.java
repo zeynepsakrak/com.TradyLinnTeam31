4f31a148d11776ec55eb25a9ac7f1b0a2060a1a5
Furkan Gungor <furkangungoronline@gmail.com>
2022-06-26 20:48:54 +0200

    26.06.22

--- src/test/java/pages/US_01_02_Page.java
+++ src/test/java/pages/US_01_02_Page.java
@@ -1,4 +1,13 @@
 package pages;
 
+import org.openqa.selenium.support.PageFactory;
+import utilities.Driver;
+
 public class US_01_02_Page {
+    public US_01_02_Page() {
+        PageFactory.initElements(Driver.getDriver(), this);
+    }
+
+
+
 }
--- src/test/java/tests/US02.java
+++ src/test/java/tests/US02.java
@@ -1,4 +1,58 @@
 package tests;
 
+import org.testng.annotations.Test;
+import pages.US_01_02_Page;
+
 public class US02 {
+    US_01_02_Page firstPage=new US_01_02_Page();
+
+
+    @Test
+    public void US01_TC01() {
+        //1_Kullanıcı https://tradylinn.com adresine gider.
+        //2_Kullanıcı Sign in butonuna tıklar.
+        //3_Kullanıcı Email adresini girer.
+        //4_Kullanıcı Password girer.
+        //5_Başarılı bir şekilde login olduğunu test eder.
+    }
+
+    @Test
+    public void US01_TC02() {
+        //1_Kullanıcı https://tradylinn.com adresine gider.
+        //2_Kullanıcı Sign in butonuna tıklar.
+        //3_Kullanıcı Email adresini girer.
+        //4_Kullanıcı Password girer.
+        //5_Orders butonunun görüldüğünü test eder.
+        //6_Downloads butonunun görüldüğünü test eder.
+        //7_Addresses butonunun görüldüğünü test eder.
+        //8_Account details butonunun görüldüğünü test eder.
+        //9_Whislist butonunun görüldüğünü test eder.
+        //10_Logout butonunun görüldüğünü test eder.
+    }
+
+    @Test
+    public void US01_TC03() {
+        //1_Kullanıcı https://tradylinn.com adresine gider.
+        //2_Kullanıcı Sign in butonuna tıklar.
+        //3_Kullanıcı Email adresini girer.
+        //4_Kullanıcı Password girer.
+        //5_Store manager butonunun görüldüğünü test eder.
+        //6_Orders butonunun görüldüğünü test eder.
+        //7_Downloads butonunun görüldüğünü test eder.
+        //8_Addresses butonunun görüldüğünü test eder.
+        //9_Account details butonunun görüldüğünü test eder.
+        //10_Appointments butonunun görüldüğünü test eder.
+        //11_Wishlist butonunun görüldüğünü test eder.
+    }
+
+    @Test
+    public void US01_TC04() {
+        //1_Kullanıcı https://tradylinn.com adresine gider.
+        //2_Kullanıcı Sign in butonuna tıklar.
+        //3_Kullanıcı Email adresini girer.
+        //4_Kullanıcı Password girer.
+        //5_Support tickets butonunun görüldüğünü test eder.
+        //6_Followings butonunun görüldüğünü test eder.
+        //7_Appointments butonunun görüldüğünü test eder.
+    }
 }
