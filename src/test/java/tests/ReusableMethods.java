96e674d6f45e369e9b5f941b1f342276bd74ef28
Resul Dagci <resuldagci27@gmail.com>
2022-06-26 09:11:52 +0200

    26.06.22

--- src/test/java/tests/ReusableMethods.java
+++ src/test/java/tests/ReusableMethods.java
@@ -5,18 +5,31 @@ import pages.Homepage;
 import utilities.ConfigReader;
 import utilities.Driver;
 
-public class ReusableMethods {
-    static  JavascriptExecutor jse = (JavascriptExecutor) Driver.getDriver();
+import static tests.Login.login;
 
-    public static void sepetiBosalt() throws InterruptedException {
-        Homepage homepage=new Homepage();
+public class ReusableMethods {
+    static JavascriptExecutor jse = (JavascriptExecutor) Driver.getDriver();
+
+    public static void sepetiBosalt() {
+        Homepage homepage = new Homepage();
         if (!homepage.sepetimIkonu.getText().contains("0")) {
             homepage.sepetimIkonu.click();
             homepage.sepetiGoruntule.click();
             jse.executeScript("arguments[0].scrollIntoView();", homepage.sepetiTemizle);
-            Thread.sleep(1000);
+            waitFor(1);
             homepage.sepetiTemizle.click();
             homepage.tradylinnIkonu.click();
+        }
+    }
+
+
+    //   HARD WAIT WITH THREAD.SLEEP
+    //   waitFor(5);  => waits for 5 second
+    public static void waitFor(int sec) {
+        try {
+            Thread.sleep(sec * 1000);
+        } catch (InterruptedException e) {
+            e.printStackTrace();
         }
     }
 }
