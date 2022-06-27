b5eadc5bfcf7b9bc4ca80a02d4923b671fc0a741
Fatih Turker <fthturker80@gmail.com>
2022-06-27 12:03:19 +0300

    US15 degisiklik

--- src/test/java/tests/US15.java
+++ src/test/java/tests/US15.java
@@ -7,58 +7,64 @@ import org.testng.annotations.Test;
 import pages.US_15_16_Page;
 import utilities.ConfigReader;
 import utilities.Driver;
+import utilities.TestBaseRapor;
 
 import static tests.ReusableMethods.waitFor;
 
 
-public class US15 {
+public class US15 extends TestBaseRapor {
     US_15_16_Page us_15_16_page=new US_15_16_Page();
 
     @Test
-    public void Test01_02_03() throws InterruptedException {
+    public void US15_TC_01_02_03() throws InterruptedException {
 
         //1- Siteye git:https://tradylinn.com/ gidilebilir
         Driver.getDriver().get(ConfigReader.getProperty("tradllyinnUrl"));
-
+        extentReports.createTest("US15_TC_01_02_03","Gecerli email ve sifre ile giris yapabilmeli");
         //2 -Vendor "My Account" butonuna tiklanir
         us_15_16_page.myAccount.click();
 
         //3- Vendor "username or email address" ve "password" bilgileri girer ve "login" butonuna tiklanir
         us_15_16_page.username.sendKeys(ConfigReader.getProperty("tradllyinnEmail"));
         us_15_16_page.password.sendKeys(ConfigReader.getProperty("tradllyinnPassword"));
-        waitFor(1);
+        ReusableMethods.waitFor(1);
         us_15_16_page.login.sendKeys(Keys.ENTER);
-
 
         //4-Vendor "Store Manager" butonuna tiklanir
         us_15_16_page.storeManager.click();
 
-
         //5-Vendor "Coupons" butonuna tiklanir
         us_15_16_page.coupons.sendKeys(Keys.ENTER);
-        ReusableMethods.bekle();
+        ReusableMethods.waitFor(10);
 
         //6- Vendor "add new "butonuna tiklanir
         us_15_16_page.addNew.click();
 
         //7- Vendor "limit" butonuna tiklanir
-        ReusableMethods.bekle();
-        Actions action=new Actions(Driver.getDriver());
+        Actions action = new Actions(Driver.getDriver());
         action.sendKeys(Keys.PAGE_DOWN).perform();
-
+        ReusableMethods.waitFor(10);
         us_15_16_page.limit.click();
 
+
         //8- Vendor "Usage limit per coupon" limiti bilgileri eklenebilir
-
-        //action.sendKeys(Keys.TAB);
         us_15_16_page.usageLimitCoupon.sendKeys("5");
-        us_15_16_page.usLimitItems.sendKeys("5");
-        us_15_16_page.perUser.sendKeys("5");
-
         Assert.assertTrue(us_15_16_page.usageLimitCoupon.getAttribute("value").contains("5"));
-        Assert.assertTrue(us_15_16_page.usLimitItems.getAttribute("value").contains("5"));
-        Assert.assertTrue(us_15_16_page.perUser.getAttribute("value").contains("5"));
-        //Driver.closeDriver();
+        Thread.sleep(2000);
+        extentReports.createTest("kupon basina kullanim limiti eklenebilir");
+
+        //8-  Vendor "Limit usage to X items" limiti bilgileri uygulanabilir
+        us_15_16_page.usLimitItems.sendKeys("15");
+        Assert.assertTrue(us_15_16_page.usLimitItems.getAttribute("value").contains("15"));
+        Thread.sleep(2000);
+        extentReports.createTest("kullanimi belirlenen urunlerle uygulanabildi");
+
+        //8- Vendor "Usage limit per user" limit bilgileri uygulanabilir
+        us_15_16_page.perUser.sendKeys("10");
+        Thread.sleep(2000);
+        Assert.assertTrue(us_15_16_page.perUser.getAttribute("value").contains("10"));
+        extentReports.createTest("kullanici basina kullanim siniri uygulanabildi");
+
+        Driver.closeDriver();
     }
 }
-
