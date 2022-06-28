5daa14695e0811152ea972514f9c2a5346f072bb
zeynepsakrak <100724081+zeynepsakrak@users.noreply.github.com>
2022-06-28 20:07:14 +0300

    US8910

--- src/test/java/tests/US08.java
+++ src/test/java/tests/US08.java
@@ -2,80 +2,59 @@ package tests;
 
 import org.apache.logging.log4j.core.config.Order;
 import org.openqa.selenium.Keys;
+import org.openqa.selenium.WebElement;
 import org.openqa.selenium.interactions.Actions;
+import org.openqa.selenium.support.ui.Select;
 import org.testng.Assert;
 import org.testng.annotations.Test;
 import pages.US_08_09_10_Page;
 import utilities.Driver;
 import utilities.TestBaseRapor;
 
-import static tests.ReusableMethods.bekle;
-import static tests.ReusableMethods.urun_ekle_menusune_gidilir;
+import java.util.ArrayList;
+import java.util.List;
 
-public class US08 extends TestBaseRapor {
+import static tests.ReusableMethods.*;
+
+public class US08 {
     US_08_09_10_Page page=new US_08_09_10_Page();
     Actions actions=new Actions(Driver.getDriver());
     @Test(priority = 1)
-    public void testName() {
+    public void InventoryGit() {
         urun_ekle_menusune_gidilir();
         bekle();
         actions.sendKeys(Keys.PAGE_DOWN).sendKeys(Keys.PAGE_DOWN).sendKeys(Keys.PAGE_DOWN).perform();
         bekle();
         page.Inventory.click();
-        bekle();
+        waitFor(3);
     }
-
-    /*
-        US08_TC01 Urun miktarı stock belirlenmeli
-    1. URL e gider
-    2. "giriş yap"  tıklanır
-    3.  geçerli email adresi ve  şifre girilir
-    4. "girişyap" tıklanır
-    5. Anasayfada Hesabım tıklanır
-    6. "Store Manager"  tıklanır
-    7.  "ürün" menüsü tıklanır
-    8.  "ürün ekle" butonu tıklanır
-    9.  "Inventory" tıklanır
-    10.  manage stock ikonu tıklanır
-    11. ürün miktarı(stock quantity) belirlenir
-
-         */
 
     @Test(priority = 2)
     public void US08_TC01() {
         //extentTest=extentReports.createTest("Pozitif Login","Gecerli username ve sifre ile giris yapabilmeli");
+        waitFor(2);
         page.ManageStock.click();
+        waitFor(5);
+        page.StockMiktar.click();
         page.StockMiktar.clear();
         page.StockMiktar.sendKeys("10");
-        bekle();
+       waitFor(5);
+       page.Submit.click();
+       waitFor(3);
         System.out.println(page.StockMiktar.getText());
         Assert.assertTrue(page.StockMiktar.getText().contains("10"));
         //extentTest.info("Urun miktarı stock belirlendi");
     }
-
-    /*
-    US08_TC02 Backorder izni verilebilmeli, gecici verilmeme optionu olmalı veya verilmeli ama müşteri bilgilendirilmeli
-1. URL e gider
-2. "giriş yap"  tıklanır
-3.  geçerli email adresi ve  şifre girilir
-4. "girişyap" tıklanır
-5. Anasayfada Hesabım tıklanır
-6. "Store Manager"  tıklanır
-7.  "ürün" menüsü tıklanır
-8. "ürün ekle" butonu tıklanır
-9.  "Inventory" pagtıklanır
-10.  manage stock ikonu tıklanır
-11. AllowBlackorder "Allow" seçilebilirliğini test eder
-12. "Allow, but notify custemar" seçeneğinin varlığını test eder
-13. "Do Not Allow" seçeneğinin varlığını kontrol eder
-
-
-     */
-
     @Test(priority = 3)
     public void US08_TC02() {
-if (!page.Allow.isSelected()){
-    page.Allow.click();
-}
+        Select select =new Select(page.AllowBlackorder);
+        List<WebElement> ActualList= select.getOptions();
+        List<WebElement> expectedList=new ArrayList<>();
+        for (WebElement each:ActualList) {
+            each.click();
+            System.out.println(each.getText());
+            expectedList.add(each);
+        }
+        Assert.assertEquals(ActualList,expectedList);
     }
 }
--- src/test/java/tests/US09.java
+++ src/test/java/tests/US09.java
@@ -4,10 +4,12 @@ import org.openqa.selenium.Keys;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.interactions.Actions;
 import org.openqa.selenium.support.ui.Select;
+import org.testng.Assert;
 import org.testng.annotations.Test;
 import pages.US_08_09_10_Page;
 import utilities.Driver;
 
+import java.util.ArrayList;
 import java.util.List;
 
 import static tests.ReusableMethods.*;
@@ -24,21 +26,6 @@ public class US09 {
         page.Shipping.click();
         bekle();
     }
-    /*
-    US09_TC01 Kilo ve boyut belirlenmeli
-1. URL ye gider
-2. "hesabım" tıklanır
-3."kullanıcı adı" ve  "password" girilir
-4. girişyap tıklanır
-5. Anasayfada "hesabım" tıklanır
-5. "Store Manager"  tıklanır
-6. "ürün" menüsü tıklanır
-7. "ürün ekle" butonu tıklanır
-8. "Shipping" butonu tıklanır
-9. Weight (kg) kutusuna kilo belirlendiğin test eder
-10. Dimensions Length değerlerinin belirlenebilirliğini test eder
-
-     */
 
     @Test(priority = 2)
     public void US09_TC01() {
@@ -46,40 +33,39 @@ public class US09 {
         page.Weight.click();
         page.Weight.clear();
         page.Weight.sendKeys("10");
-        bekle();
-        bekle();
+
+        waitFor(8);
         page.Length.click();
         page.Length.clear();
         page.Length.sendKeys("20");
-        bekle();
-        bekle();
+        //Assert.assertTrue(page.Length.getText().contains("20"));
+        waitFor(10);
         page.height.click();
         page.height.clear();
         page.height.sendKeys("30");
+        //Assert.assertTrue(page.height.getText().contains("30"));
+        page.width.click();
+        page.width.clear();
+        page.width.sendKeys("30");
+        //Assert.assertTrue(page.width.getText().contains("30"));
+        actions.sendKeys(Keys.PAGE_DOWN).perform();
+        waitFor(3);
+        Assert.assertTrue(page.Length.getText().contains("20"));
+        waitFor(3);
+
     }
-/*
-    US09_TC02 Hacim ve ne kadar surede teslimat yapilacagi belirlenmeli
-1. URL ye gider
-2. "hesabım" tıklanır
-3."kullanıcı adı" ve  "password" girilir
-4. girişyap tıklanır
-5. Anasayfada "hesabım" tıklanır
-6. "Store Manager"  tıklanır
-7. "ürün" menüsü tıklanır
-8. "ürün ekle" butonu tıklanır
-9. "Shipping" butonu tıklanır
-10. Hacim seçeneğinin varlığını test eder
-11. Processing Time seçeneğinden süre belirlenmeli
-     */
 
     @Test(priority = 3)
     public void US09_TC02() {
         Select select =new Select(page.processingTime);
         List<WebElement> processingTime= select.getOptions();
+        List<WebElement> expectedList=new ArrayList<>();
         for (WebElement each:processingTime) {
             each.click();
             System.out.println(each.getText());
+            expectedList.add(each);
         }
+        Assert.assertEquals(processingTime,expectedList);
 
     }
 }
