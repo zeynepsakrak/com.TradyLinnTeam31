4ee71552ae32ef1b8452456cda89bf1a9e537030
zeynepsakrak <100724081+zeynepsakrak@users.noreply.github.com>
2022-06-30 00:47:58 +0300

    driver

--- src/test/java/tests/Login.java
+++ src/test/java/tests/Login.java
@@ -5,14 +5,18 @@ import pages.Homepage;
 import utilities.ConfigReader;
 import utilities.Driver;
 
+import static tests.ReusableMethods.waitFor;
+
 
 public class Login {
     public static void login() {
         Homepage homepage = new Homepage();
         Driver.getDriver().get(ConfigReader.getProperty("tradylinnUrl"));
         homepage.girisButonu.click();
+        waitFor(3);
         homepage.userName.sendKeys(ConfigReader.getProperty("validVendorEmail"));
         homepage.pasword.sendKeys(ConfigReader.getProperty("validVendorPassword"));
+        waitFor(3);
         homepage.girisYapButonu.click();
     }
 }
