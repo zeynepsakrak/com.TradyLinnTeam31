678e89a61216546035c25c60a51a61cb9ab21947
ilker61 <jakyuer@gmail.com>
2022-06-30 12:13:19 +0200

    SmokeTest Class

--- src/test/java/tests/E2E.java
+++ src/test/java/tests/E2E.java
@@ -200,44 +200,41 @@ public class E2E extends TestBaseRapor {
         extentTest.info("'Minimum spend' alanına minimum satın alma miktarı girilebiliyor.");
     }
     @Test
-    public void US15_TC_01_02_03() throws InterruptedException {
-        //1- Siteye git:https://tradylinn.com/ gidilebilir
+    public void US15_TC_01_02_03() throws InterruptedException, IOException {
         Driver.getDriver().get(ConfigReader.getProperty("tradylinnUrl"));
-        extentReports.createTest("US15_TC_01_02_03","Gecerli email ve sifre ile giris yapabilmeli");
-        //2 -Vendor "My Account" butonuna tiklanir
+        extentTest=extentReports.createTest("US15_TC_01_02_03","Gecerli email ve sifre ile giris yapabilmeli");
         us_15_16_page.myAccount.click();
-        //3- Vendor "username or email address" ve "password" bilgileri girer ve "login" butonuna tiklanir
+        extentTest.info("myAccount butonu basilabildi");
         us_15_16_page.username.sendKeys(ConfigReader.getProperty("validVendorEmail"));
         us_15_16_page.password.sendKeys(ConfigReader.getProperty("validVendorPassword"));
         ReusableMethods.waitFor(1);
         us_15_16_page.login.sendKeys(Keys.ENTER);
-        //4-Vendor "Store Manager" butonuna tiklanir
+        extentTest.info("login butonu basilabildi");
         us_15_16_page.storeManager.click();
-        //5-Vendor "Coupons" butonuna tiklanir
+        extentTest.info("Vendor store manager butonu basilabildi");
         us_15_16_page.coupons.sendKeys(Keys.ENTER);
         ReusableMethods.waitFor(10);
-        //6- Vendor "add new "butonuna tiklanir
+        extentTest.info("Vendor coupons butonu basilabildi");
         us_15_16_page.addNew.click();
-        //7- Vendor "limit" butonuna tiklanir
+        extentTest.info("Vendor butonu basilabildi");
         Actions action = new Actions(Driver.getDriver());
         action.sendKeys(Keys.PAGE_DOWN).perform();
         ReusableMethods.waitFor(10);
         us_15_16_page.limit.click();
-        //8- Vendor "Usage limit per coupon" limiti bilgileri eklenebilir
+        extentTest.info("Vendor limit butonu basilabildi");
         us_15_16_page.usageLimitCoupon.sendKeys("5");
         Assert.assertTrue(us_15_16_page.usageLimitCoupon.getAttribute("value").contains("5"));
         Thread.sleep(2000);
         extentReports.createTest("kupon basina kullanim limiti eklenebilir");
-        //8-  Vendor "Limit usage to X items" limiti bilgileri uygulanabilir
         us_15_16_page.usLimitItems.sendKeys("15");
         Assert.assertTrue(us_15_16_page.usLimitItems.getAttribute("value").contains("15"));
         Thread.sleep(2000);
         extentReports.createTest("kullanimi belirlenen urunlerle uygulanabildi");
-        //8- Vendor "Usage limit per user" limit bilgileri uygulanabilir
         us_15_16_page.perUser.sendKeys("10");
         Thread.sleep(2000);
         Assert.assertTrue(us_15_16_page.perUser.getAttribute("value").contains("10"));
         extentReports.createTest("kullanici basina kullanim siniri uygulanabildi");
+        ReusableMethods.getScreenshot("Limit uygulama");
         Driver.closeDriver();
     }
 }
